809632c1e03380a4c0f2e78dd4ac345c7c4d99f7
kevvykev <kevyg123@gmail.com>
2015-02-12 18:55:35 +0000

    customized my app

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
 #! /usr/bin/env python
 import os
 from app import app
-app.run(debug=True,host="0.0.0.0",port=8080)
+app.run(debug=True, host=os.getenv("IP", '0.0.0.0'),port=int(os.getenv("PORT", 8080) ))
