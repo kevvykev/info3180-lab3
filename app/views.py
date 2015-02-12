e772a3358aba7524df94addc94b1de9274459fec
kevvykev <kevyg123@gmail.com>
2015-02-12 18:05:51 +0000

    customized my app

--- app/views.py
+++ app/views.py
@@ -5,7 +5,7 @@ Werkzeug Documentation:  http://werkzeug.pocoo.org/documentation/
 
 This file creates your application.
 """
-
+import smtplib
 from app import app
 from flask import render_template, request, redirect, url_for
 
@@ -13,6 +13,43 @@ from flask import render_template, request, redirect, url_for
 ###
 # Routing for your application.
 ###
+
+def sendmail(fromname,fromaddr,subject,msg):
+  # Credentials (if needed)
+
+  username = 'kevyg123@gmail.com'
+
+  password = 'hbodojywustkxeou'
+
+  # The actual mail send
+
+  server = smtplib.SMTP('smtp.gmail.com:587')
+
+  server.starttls()
+  server.login(username,password)
+  toaddrs = 'kevyg123@gmail.com'
+  message = """From: {} <{}>
+
+  To: {} <{}>
+
+  Subject: {}
+
+  {}
+
+  """
+  toname = 'David Bain'
+
+  messagetosend = message.format(
+   fromname,
+   fromaddr,
+   toname,
+   toaddrs,
+   subject,
+   msg)
+  server.sendmail(fromaddr, toaddrs, messagetosend)
+
+  server.quit()
+  return True
 
 @app.route('/')
 def home():
@@ -36,6 +73,23 @@ def send_text_file(file_name):
     file_dot_text = file_name + '.txt'
     return app.send_static_file(file_dot_text)
 
+@app.route('/contact/', methods=["POST", "GET"])
+def contact():
+  """Render webpage contact"""
+  if request.method == "POST":
+    name = request.form["name"]
+    email = request.form["email"]
+    subject = request.form["subject"]
+    message = request.form["message"]
+    
+    if sendmail(fromname=name,
+                fromaddr=email,
+                subject=subject,
+                msg=message):
+      return render_template("contact.html",
+                             status="Mail sent"
+                            )
+  return render_template('contact.html')
 
 @app.after_request
 def add_header(response):
@@ -55,4 +109,4 @@ def page_not_found(error):
 
 
 if __name__ == '__main__':
-    app.run(debug=True,host="0.0.0.0",port="8888")
+    app.run(debug=True,host="0.0.0.0",port="8080")
--- run.py
+++ run.py
@@ -1,3 +1,4 @@
 #! /usr/bin/env python
+import os
 from app import app
 app.run(debug=True,host="0.0.0.0",port=8080)
